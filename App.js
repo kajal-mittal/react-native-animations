e0abd4617a76c09dcea49f477266d3e60af89e7a
Punchh_Kajal <punchh_kajal@Punchh-Kajals-MacBook-Pro.local>
2018-08-20 22:56:14 +0530

    -minor changes into flow.

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { PureComponent } from 'react';
-import { Platform, StyleSheet, Text, View, Animated, TouchableOpacity, PanResponder, Easing } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 const instructions = Platform.select({
 	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,71 +15,27 @@ const instructions = Platform.select({
 });
 
 export default class App extends PureComponent {
-	state = {
-		animBgColor: new Animated.Value(0),
-		animRotate: new Animated.Value(0),
-		animScale: new Animated.Value(1),
-		width: new Animated.Value(100)
-	};
-	constructor(props) {
-		super(props);
-		this.startAnimation - this.startAnimation.bind(this);
-	}
-
-	startAnimation = () => {
-		Animated.parallel([
-			Animated.timing(this.state.animRotate, {
-				toValue: 360,
-				easing: Easing.bounce,
-				duration: 500
-			}),
-			Animated.timing(this.state.animBgColor, {
-				toValue: 1,
-				easing: Easing.bounce,
-				duration: 200
-			}),
-			Animated.timing(this.state.animScale, {
-				toValue: 2,
-				easing: Easing.bounce,
-				duration: 200
-			})
-		]).start(() => {
-			this.state.animBgColor.setValue(0);
-			this.state.animRotate.setValue(0);
-			this.state.animScale.setValue(1);
-		});
-	};
 	render() {
-		const rotateInterpolate = this.state.animRotate.interpolate({
-			inputRange: [0, 360],
-			outputRange: ['0deg', '1080deg']
-		});
-		const bgInterpolate = this.state.animBgColor.interpolate({
-			inputRange: [0, 1],
-			outputRange: ['red', 'green']
-		});
-		// Destructure the value of pan from the state
-		const animatedStyle = {
-			backgroundColor: bgInterpolate,
-			width: this.state.width,
-			height: this.state.width,
-			transform: [
-				{
-					rotate: rotateInterpolate
-				},
-				{
-					scale: this.state.animScale
-				}
-			]
-		};
-
 		return (
 			<View style={styles.container}>
-				<Animated.View style={[styles.box, animatedStyle]}>
-					<TouchableOpacity onPress={this.startAnimation} style={[styles.box]}>
-						<Text style={styles.instructions}>{instructions}</Text>
-					</TouchableOpacity>
-				</Animated.View>
+				<Button
+					title={'PanResponder'}
+					onPress={() => {
+						this.props.navigation.navigate('PanResponderScene');
+					}}
+				/>
+				<Button
+					title={'ScaleAnimation'}
+					onPress={() => {
+						this.props.navigation.navigate('ScaleAnimation');
+					}}
+				/>
+				<Button
+					title={'RotateAnimation'}
+					onPress={() => {
+						this.props.navigation.navigate('RotateAnimation');
+					}}
+				/>
 			</View>
 		);
 	}
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 /** @format */
-
-import { AppRegistry } from 'react-native';
+import React, { PureComponent } from 'react';
+import { AppRegistry, View } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import PanResponderScene from './components/PanResponderScene';
 import ScaleAnimation from './components/ScaleAnimation';
+import RotateAnimation from './components/RotateAnimation';
+import { StackNavigator } from 'react-navigation';
 
-AppRegistry.registerComponent(appName, () => ScaleAnimation);
+const AppFlow = StackNavigator({
+	App: { screen: App },
+	RotateAnimation: { screen: RotateAnimation },
+	ScaleAnimation: { screen: ScaleAnimation },
+	PanResponderScene: { screen: PanResponderScene }
+});
+
+export default class FirstScene extends PureComponent {
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				<AppFlow />
+			</View>
+		);
+	}
+}
+
+AppRegistry.registerComponent(appName, () => FirstScene);
